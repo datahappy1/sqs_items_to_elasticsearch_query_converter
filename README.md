6a56853eb79e8044e25f32696a544ce63700de4f
datahappy1 <43505494+datahappy1@users.noreply.github.com>
2020-10-31 00:30:24 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# sqs_items_to_elasticsearch_query_converter
+# sqs_to_kibana_query_filter_helper
+This helper tool polls for messages in a specified AWS SQS queue, filters these messages by a "key" name declares as a variable, and returns an Elastic DSL query filter string for your Kibana search, that is prepared for searching the IDs using a `should` statement.
+
+# How to run this tool:
+1) Git clone this tool and ideally setup a Pipenv or a plain virtual environment.
+2) Setup these environment variables with your AWS access key ID and the secret access key:
+    - `aws_access_key_id`
+    - `aws_secret_access_key`
+3) Setup these variables in `__main__.py`:
+    - `AWS_REGION_NAME` string, for example: `us-east-1`
+    - `QUEUE_URL` string, for example: `https://sqs.us-east-1.amazonaws.com/123456789012/MyQueue`
+    - `QUEUE_VISIBILITY_TIMEOUT_SECONDS` integer, i.e `30` seconds of visibility timeout
+    - `COMMON_SQS_AND_ELASTIC_KEY` string, for example `PersonId` ( This is a key in your data that has to be common for the SQS message body and the indexed field in your Elasticsearch index )
+4) Run the `__main__.py` script
+5) Use the result JSON in your Kibana search filter DSL query ![](/docs/img/DSL_query_filter.PNG)
+
+# Useful links:
+- https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sqs.html
+- https://www.elastic.co/guide/en/elasticsearch/reference/7.9/query-filter-context.html
---
+++ __main__.py
@@ -0,0 +1,100 @@
+import json
+import boto3
+from os import environ
+
+AWS_ACCESS_KEY_ID = environ['aws_access_key_id']
+AWS_SECRET_ACCESS_KEY = environ['aws_secret_access_key']
+AWS_REGION_NAME = 'us-east-1'
+QUEUE_URL = "https://sqs.us-east-1.amazonaws.com/..."
+QUEUE_VISIBILITY_TIMEOUT_SECONDS = 30
+
+COMMON_SQS_AND_ELASTIC_KEY = "SomeId"
+
+
+class SqsMessage:
+    def __init__(self):
+        self.session = boto3.Session(aws_access_key_id=AWS_ACCESS_KEY_ID,
+                                     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                                     region_name=AWS_REGION_NAME)
+        self.sqs = self.session.client('sqs')
+
+    def _receive_message(self):
+        # Receive message from SQS queue
+        response = self.sqs.receive_message(
+            QueueUrl=QUEUE_URL,
+            AttributeNames=[
+                'SentTimestamp'
+            ],
+            MaxNumberOfMessages=1,
+            MessageAttributeNames=[
+                'All'
+            ],
+            VisibilityTimeout=QUEUE_VISIBILITY_TIMEOUT_SECONDS,
+            WaitTimeSeconds=0
+        )
+
+        try:
+            _message = response['Messages'][0]
+        except KeyError:
+            return None
+
+        return _message
+
+    def receive_all_messages(self):
+        all_messages = []
+        while True:
+            _message_resp = self._receive_message()
+            if not _message_resp:
+                break
+            all_messages.append(_message_resp)
+
+        return all_messages
+
+
+def parse_attribute_from_message(message, attribute_name):
+    try:
+        message_body_dict = json.loads(message["Body"])
+        message_dict = json.loads(message_body_dict["Message"])
+        message_resource_dict = message_dict.get("resource")
+    except KeyError as key_err:
+        raise key_err
+    return message_resource_dict[attribute_name]
+
+
+def get_dsl_query_filter_base():
+    return {
+        "bool": {
+            "should": None,
+        }
+    }
+
+
+def add_item_to_dsl_query_filter_should(item):
+    elastic_dsl_should = {
+        "term": {
+            COMMON_SQS_AND_ELASTIC_KEY: str(item)
+        }
+    }
+    return elastic_dsl_should
+
+
+if __name__ == "__main__":
+    message = SqsMessage()
+    received_messages = message.receive_all_messages()
+    print('Received %s messages' % len(received_messages))
+
+    attribute_found_items_list = []
+    for received_message in received_messages:
+        attribute_found_items_list.append(parse_attribute_from_message(received_message,
+                                                                       COMMON_SQS_AND_ELASTIC_KEY))
+
+    should_items_list = []
+    for item in attribute_found_items_list:
+        should_items_list.append(add_item_to_dsl_query_filter_should(item))
+
+    elastic_dsl_base_query = get_dsl_query_filter_base()
+    elastic_dsl_base_query["bool"]["should"] = should_items_list
+
+    print('Add this JSON to DSL query filter in your Kibana search:')
+    print('--------------------------------------------------------')
+    print(json.dumps(elastic_dsl_base_query))
